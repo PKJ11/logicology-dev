8397d01892d585e08c6a9c3566d87a6aa7a13d78
PKJ11 <108004611+PKJ11@users.noreply.github.com>
2025-11-24 14:27:47 +0530

    created logicoland-volume-1 page

---
+++ app/logicoland-volume-1/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logicoland1Page from "../books/logicoland/page";
+
+const Logicoland = () => {
+  return (
+    <div>
+      <Logicoland1Page />
+    </div>
+  );
+};
+
+export default Logicoland;
